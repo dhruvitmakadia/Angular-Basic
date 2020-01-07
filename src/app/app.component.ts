18728b316a5a0b053caf82157b6305032a6a7d45
Dhruvit Makadia <dhruvit.makadia@cs33-PC.1rivet.local>
2020-01-07 14:47:59 +0530

    Final Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { MyserviceService } from './myservice.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  interpolation: ['::', '::']
 })
+
 export class AppComponent {
-  title = 'myapp';
+
+  title = 'This Is My Application';
+  name = 'Dhruvit Makadia';
+  number = 9426877444;
+  names = ['My', 'Name', 'Is', 'Dhruvit'];
+  flag: boolean;
+  constructor(public myservice: MyserviceService) {
+    myservice.alertName(this.name);
+  }
+  sayHello() {
+    alert('Hello !');
+  }
+
 }
